dec38a32221914c4dc46fbfcebdd876445a2c67c
Pavlo_Zarudnii <pavlo_zarudnii@epam.com>
2016-10-18 16:25:25 +0300

    Init

---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    templateUrl: '/app/view/comp.html',//'<h1>My First Angular</h1>'
+})
+export class AppComponent { }
